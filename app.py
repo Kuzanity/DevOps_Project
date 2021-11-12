5cc16d6748622e5abcb43f08265540c68812079f
Kuzanity <adriangabrieldiaz07@gmail.com>
2021-11-12 05:15:25 +0000

    Initial project using Flask

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+@app.route('/main')
+def main():
+    return render_template('main.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=3030)
