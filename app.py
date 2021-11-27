fbb962ee55a3f54fbfdf6c5cc5ce183c65814cc3
Adrian Diaz <51974094+Kuzanity@users.noreply.github.com>
2021-11-27 23:36:44 +0800

    Midterm Activity part 2

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import sqlite3
+conn = sqlite3.connect('database.db')
+
+#conn.execute('CREATE TABLE users (email TEXT, password TEXT)')
+
+conn.close()
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-@app.route('/')
+@app.route('/home')
 def index():
     return render_template('home.html')
 @app.route('/main')
@@ -12,6 +18,13 @@ def main():
 @app.route('/about')
 def about():
     return render_template('about.html')
+@app.route('/')
+def login():
+    return render_template('login.html')
+@app.route('/regis')
+def register():
+
+    return render_template('register.html')
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=8000)
